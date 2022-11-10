9fe545fea1023848e56564d62bdf33de68bb083a
Roey30 <113986181+Roey30@users.noreply.github.com>
2022-11-10 20:31:46 +0200

    Update final_server.py

--- final_server.py
+++ final_server.py
@@ -10,7 +10,7 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 LIST_THREADS = []
 
 EXAMPLE = 'EC9C0F7EDCC18A98B1F31853B1813301'
-EXAMPLE2 = 'd18f655c3fce66ca401d5f38b48c89af'  # 479
+EXAMPLE2 = 'b7a782741f667201b54880c925faec4b'  # 10000
 MAX_NUMBER = 1000
 MAX_CLIENTS = 50
 CLIENT_NUMBER = 0
@@ -24,16 +24,19 @@ def main():
     try:
         server_socket.bind((IP, PORT))
         server_socket.listen(QUEUE_SIZE)
-        print("Listening for connections on port %d" % PORT)
-        while CLIENT_NUMBER < MAX_CLIENTS and found == False:
-            client_socket, client_address = server_socket.accept()
-            thread = Thread(target=handle_clients, args=(client_socket, client_address))
-            LIST_THREADS.append(thread)
-            CLIENT_NUMBER += 1
-            print(f'List of threads: {LIST_THREADS}')
-            print(f'THe number of clients is - {CLIENT_NUMBER}')
-            thread.run()
-            if did_found == '1':
+        while True:
+            # print("Listening for connections on port %d" % PORT)
+            while CLIENT_NUMBER < MAX_CLIENTS and found == False:
+                client_socket, client_address = server_socket.accept()
+                thread = Thread(target=handle_clients, args=(client_socket, client_address))
+                LIST_THREADS.append(thread)
+                CLIENT_NUMBER += 1
+                print(f'List of threads: {LIST_THREADS}')
+                print(f'THe number of clients is - {CLIENT_NUMBER}')
+                thread.start()
+                if did_found != 'False':
+                    exit()
+            if did_found != 'False':
                 exit()
     except socket.error as err:
         print('received socket exception - ' + str(err))
@@ -42,24 +45,25 @@ def main():
 
 
 def handle_clients(client_socket, client_address):
-    global did_found
+    global did_found, CLIENT_NUMBER
     while did_found == 'False':
         print(f'hello client - {client_address} from the socket - {client_socket}')
         first_range_of_number = math.trunc((MAX_NUMBER / 50) * (CLIENT_NUMBER - 1))
         final_range_of_number = math.trunc((MAX_NUMBER / 50) * CLIENT_NUMBER)
+        CLIENT_NUMBER += 1
         range_number = str(first_range_of_number) + '-' + str(final_range_of_number)
-        print(f'The first range - {first_range_of_number}')
-        print(f'The final range - {final_range_of_number}')
-        print(f'Sending the word - {EXAMPLE2}')
-        print(f'Sending the range - {range_number}')
-        message = EXAMPLE, str(range_number)
+        print(f'The first range: {first_range_of_number}')
+        print(f'The final range: {final_range_of_number}')
+        print(f'Sending the word: {EXAMPLE2}')
+        print(f'Sending the range: {range_number}')
+        message = EXAMPLE2, str(range_number)
         print(message)
         client_socket.send(message[0].encode() + ','.encode() + message[1].encode())
         did_found = client_socket.recv(MaxPacket).decode()
         print(did_found)
         if did_found == 'False':
-            print('The client did not found it')
-        else:
+            print(f'The client did not found it {client_address}')
+        elif did_found == 'True':
             print('The client did found it')
             return
 
