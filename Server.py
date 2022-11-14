25f9673a1817cdbf840b8b787e82033d726c0990
Roey30 <113986181+Roey30@users.noreply.github.com>
2022-11-14 10:34:46 +0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -6,13 +6,12 @@ import socket
 import math
 from threading import *
 
-
 # CONSTANTS
-MAXPACKETS = 1024
+MAX_PACKETS = 1024
 QUEUE_SIZE = 10
 IP = '0.0.0.0'
-PORT = 3256
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+PORT = 32564
+SERVER_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 LIST_THREADS = []
 
 WORD_TO_FIND = 'EC9C0F7EDCC18A98B1F31853B1813301'
@@ -21,29 +20,39 @@ MAX_NUMBER = 1000
 MAX_CLIENTS = 50
 CLIENT_NUMBER = 0
 
+MSG = ''
 DID_FOUND = 'False'
-DISCOVERD = False
+DISCOVERED = False
 
 
 # FUNCTIONS
 
 
+def range_giver():
+    first_range_of_number = math.trunc((MAX_NUMBER / MAX_CLIENTS) * (CLIENT_NUMBER - 1))
+    final_range_of_number = math.trunc((MAX_NUMBER / MAX_CLIENTS) * CLIENT_NUMBER)
+    return first_range_of_number, final_range_of_number
+
+
 def handle_clients(client_socket, client_address):
-    global DID_FOUND, CLIENT_NUMBER, DISCOVERD
-    while not DISCOVERD:
-        first_range_of_number = math.trunc((MAX_NUMBER / 50) * (CLIENT_NUMBER - 1))
-        final_range_of_number = math.trunc((MAX_NUMBER / 50) * CLIENT_NUMBER)
+    global DID_FOUND, CLIENT_NUMBER, DISCOVERED, MSG
+    while not DISCOVERED:
+        first_range_of_number = range_giver()[0]
+        final_range_of_number = range_giver()[1]
         CLIENT_NUMBER += 1
         range_number = str(first_range_of_number) + '-' + str(final_range_of_number)
         message = EXAMPLE, str(range_number)
         client_socket.send(message[0].encode() + ','.encode() + message[1].encode())
-        if not DISCOVERD:
-            DID_FOUND = client_socket.recv(MAXPACKETS).decode()
+        if not DISCOVERED:
+            MSG = client_socket.recv(MAX_PACKETS).decode()
+            MSG = MSG.split(',')
+            DID_FOUND = MSG[0]
         if DID_FOUND == 'False':
             pass
         elif DID_FOUND == 'True':
-            print(f'The client {client_address} found it ')
-            DISCOVERD = True
+            print(f'The client {client_address} found it, it was the hash of number {MSG[1]} ')
+            DISCOVERED = True
+            return
 
 
 def main():
@@ -53,27 +62,27 @@ def main():
     """
     global CLIENT_NUMBER, DID_FOUND
     try:
-        server_socket.bind((IP, PORT))
-        server_socket.listen(QUEUE_SIZE)
+        SERVER_SOCKET.bind((IP, PORT))
+        SERVER_SOCKET.listen(QUEUE_SIZE)
         print('Starting process')
-        while CLIENT_NUMBER < MAX_CLIENTS and DISCOVERD == False:
-            client_socket, client_address = server_socket.accept()
+        while CLIENT_NUMBER < MAX_CLIENTS and not DISCOVERED:
+            client_socket, client_address = SERVER_SOCKET.accept()
             print(f'Hello client {client_address}')
-            thread = Thread(target=handle_clients, args=(client_socket, client_address), daemon=True)
+            thread = Thread(target=handle_clients, args=(client_socket, client_address))
             LIST_THREADS.append(thread)
             CLIENT_NUMBER += 1
             thread.start()
         for t in LIST_THREADS:
             t.join()
-        if DISCOVERD:
-            exit(f'The client found it ')
+        if DISCOVERED:
+            exit()
         else:
             print('The client did not found it')
     except socket.error and ConnectionAbortedError as err:
         print('Some problem came up - ' + str(err))
     finally:
-        server_socket.close()
-        if DISCOVERD:
+        SERVER_SOCKET.close()
+        if DISCOVERED:
             print('Done')
 
 
