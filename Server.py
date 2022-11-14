d1945298d9126f8c0cdca404a5119ae23b34a5d3
Roey30 <113986181+Roey30@users.noreply.github.com>
2022-11-14 10:54:52 +0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,5 +1,10 @@
 """
-
+Name : Roey Firan
+Program name : Project MD5
+Date : 14/11/2022
+Description: Uses to find a reverse hash function,
+gets a sting and returns the number that if you do the hash function on you
+get the string
 """
 # IMPORTS
 import socket
@@ -15,7 +20,6 @@ SERVER_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 LIST_THREADS = []
 
 WORD_TO_FIND = 'EC9C0F7EDCC18A98B1F31853B1813301'
-EXAMPLE = '16583616b2cb7d6cd3393bb2e3694c26'  # 76859
 MAX_NUMBER = 1000
 MAX_CLIENTS = 50
 CLIENT_NUMBER = 0
@@ -29,6 +33,10 @@ DISCOVERED = False
 
 
 def range_giver():
+    """
+    calculates the range
+    :return: first range and last range
+    """
     first_range_of_number = math.trunc((MAX_NUMBER / MAX_CLIENTS) * (CLIENT_NUMBER - 1))
     final_range_of_number = math.trunc((MAX_NUMBER / MAX_CLIENTS) * CLIENT_NUMBER)
     return first_range_of_number, final_range_of_number
@@ -41,7 +49,7 @@ def handle_clients(client_socket, client_address):
         final_range_of_number = range_giver()[1]
         CLIENT_NUMBER += 1
         range_number = str(first_range_of_number) + '-' + str(final_range_of_number)
-        message = EXAMPLE, str(range_number)
+        message = WORD_TO_FIND, str(range_number)
         client_socket.send(message[0].encode() + ','.encode() + message[1].encode())
         if not DISCOVERED:
             MSG = client_socket.recv(MAX_PACKETS).decode()
@@ -57,8 +65,9 @@ def handle_clients(client_socket, client_address):
 
 def main():
     """
-
-    :return:
+    The main function:
+    connect to the client and open the threads for each client
+    for each client sends the range and prints the answer
     """
     global CLIENT_NUMBER, DID_FOUND
     try:
